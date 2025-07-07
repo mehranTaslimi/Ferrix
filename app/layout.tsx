228d36c861fa4c0d657df39725ea158e49f0cdaa
MehranTaslimi <mehrantaslimi92@gmail.com>
2025-07-07 15:57:39 +0330

    feat: add resize for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from "@/components/theme-provider"
 import { DownloadProvider } from "@/components/download-context"
 import Sidebar from "@/components/sidebar"
 import "./globals.css"
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
+import { Input } from "@/components/ui/input"
 
 export default function RootLayout({
   children,
@@ -23,12 +25,21 @@ export default function RootLayout({
           <DownloadProvider>
             <div className="h-screen overflow-hidden">
               <div className="h-full overflow-y-auto">
-                <div className="fixed w-full h-8 bg-transparent left-0 top-0 z-auto" data-tauri-drag-region />
-                <div className="flex w-screen h-screen">
-                  <div className="basis-64 overflow-x-hidden overflow-y-auto">
-                    <Sidebar />
-                  </div>
-                  <div className="bg-background flex-1 overflow-x-hidden overflow-y-auto p-8">{children}</div>
+                <div className="fixed w-full h-6 bg-transparent left-0 top-0 z-50" data-tauri-drag-region />
+                <div className="w-screen h-screen">
+                  <ResizablePanelGroup direction="horizontal">
+                    <ResizablePanel defaultSize={25} minSize={10} maxSize={40}>
+                      <div className="overflow-x-hidden overflow-y-auto">
+                        <Sidebar />
+                      </div>
+                    </ResizablePanel>
+                    <ResizableHandle className="bg-transparent" />
+                    <ResizablePanel defaultSize={75}>
+                      <div className="h-screen flex items-center mr-3">
+                        <div className="bg-background w-full max-h-[96%] rounded-md p-3 overflow-x-hidden overflow-y-auto">{children}</div>
+                      </div>
+                    </ResizablePanel>
+                  </ResizablePanelGroup>
                 </div>
               </div>
             </div>
